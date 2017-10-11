046568030c2937afc23e40a91fc4b8b5c0ddc8de
attitude <937316808@qq.com>
2017-10-12 00:23:45 +0800

    delete

--- MovieRecSys/recSys/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from recSys.models import *
-# Register your models here.
-
-
-admin.site.register(Movie)
-admin.site.register(Love)
-admin.site.register(ExtUser)
-admin.site.register(Comment)
